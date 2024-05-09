3a2ccb660a963ade52efd65ad37a1cf571f94786
FrancesscoMoroni <bartekk58@gmail.com>
2024-05-09 17:26:52 +0200

    Nauka Compose

--- DesktopApp/build.gradle.kts
+++ DesktopApp/build.gradle.kts
@@ -10,6 +10,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://maven.google.com/")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
@@ -19,6 +20,10 @@ dependencies {
     // compose.desktop.currentOs should be used in launcher-sourceSet
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.material3)
+
+    implementation(compose.material3)
+    implementation(compose.materialIconsExtended)
     implementation(compose.desktop.currentOs)
 }
 
--- DesktopApp/src/main/kotlin/Main.kt
+++ DesktopApp/src/main/kotlin/Main.kt
@@ -1,33 +1,93 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
-fun App() {
+fun App(Win window) {
+    Button(onClick = {window}) {Text("FullScreen")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*val colors = listOf(
+        Color(0xFFffd7d7.toInt()),
+        Color(0xFFffe9d6.toInt()),
+        Color(0xFFfffbd0.toInt()),
+        Color(0xFFe3ffd9.toInt()),
+        Color(0xFFd0fff8.toInt())
+    )
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     var text by remember { mutableStateOf("Hello, World!") }
 
-    val scanner = Scanner(System.`in`)
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+
+        var selectedItem by remember { mutableIntStateOf(0) }
+        val items = listOf("Losowanie", "Mapa", "Ekwipunek")
+        val icons = listOf(Icons.Filled.Casino, Icons.Filled.Map, Icons.Filled.Backpack)
+
+        Scaffold(topBar = {
+            TopAppBar(title = { Text("Testowa aplikacja") })
+        }, content = {
+            NavigationRail {
+                items.forEachIndexed { index, item ->
+                    NavigationRailItem(
+                        icon = { Icon(icons[index], contentDescription = item) },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
+                    )
+                }
+            }
+        })
+
+    }*/
+}
+
+fun test() {
+
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+
+    Window(onCloseRequest = ::exitApplication,
+        title = "RPGDesktop",
+        state = rememberWindowState(placement = WindowPlacement.Maximized),
+        undecorated = true
+    ) {
+        App(window)
     }
 }
